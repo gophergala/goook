fb4b600b6ea86d89f9028ceaa8d0e06fb9758488
Dan Forest <bonjour@dan.tf>
2015-01-24 19:39:23 +0100

    Initial naive bayes

--- bayes.go
+++ bayes.go
@@ -1,6 +1,39 @@
 package main
 
-type NaiveBayes interface {
-	Learn(class string, words []string)
-	Classify(words []string) map[string]float64
+type NaiveBayes map[string]map[string]uint
+
+func (nb NaiveBayes) Learn(class string, words []string) {
+	for _, word := range words {
+		wordModel, exists := nb[word]
+		if !exists {
+			wordModel = make(map[string]uint)
+			nb[word] = wordModel
+		}
+
+		wordModel[class]++
+	}
+}
+
+func (nb NaiveBayes) Classify(words []string) map[string]float64 {
+	total := uint(0)
+	counts := make(map[string]uint)
+
+	for _, word := range words {
+		wordModel, exists := nb[word]
+		if !exists {
+			continue
+		}
+
+		for class, count := range wordModel {
+			counts[class] += count
+			total += count
+		}
+	}
+
+	probabilities := make(map[string]float64)
+	for class, count := range counts {
+		probabilities[class] = float64(count) / float64(total)
+	}
+
+	return probabilities
 }
--- main.go
+++ main.go
@@ -11,9 +11,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(ddc)
+	_ = ddc
 
 	test := Class{111, "Hello"}
 	fmt.Println(test)
 	fmt.Println(test.Depth())
+
+	nb := make(NaiveBayes)
+	nb.Learn("ham", []string{"hello", "world", "fun"})
+	nb.Learn("spam", []string{"viagra", "fat", "prince"})
+
+	fmt.Println(nb.Classify([]string{"hello", "world"}))
+	fmt.Println(nb.Classify([]string{"hello", "viagra"}))
+	fmt.Println(nb.Classify([]string{"viagra", "prince"}))
 }
